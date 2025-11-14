63f93a4ab6209119045bbdd02bf32c7319b98710
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2025-11-14 12:07:55 -0600

    Cambios en clase

--- EUCArden/Intermediate/Build/Win64/UnrealEditor/Inc/EUCArden/UHT/EUCArden.init.gen.cpp
+++ EUCArden/Intermediate/Build/Win64/UnrealEditor/Inc/EUCArden/UHT/EUCArden.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeEUCArden_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB954B62F,
-				0x5089964D,
+				0xAD9F8F9B,
+				0xB1E157AF,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_EUCArden.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_EUCArden.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_EUCArden(Z_Construct_UPackage__Script_EUCArden, TEXT("/Script/EUCArden"), Z_Registration_Info_UPackage__Script_EUCArden, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB954B62F, 0x5089964D));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_EUCArden(Z_Construct_UPackage__Script_EUCArden, TEXT("/Script/EUCArden"), Z_Registration_Info_UPackage__Script_EUCArden, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAD9F8F9B, 0xB1E157AF));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
